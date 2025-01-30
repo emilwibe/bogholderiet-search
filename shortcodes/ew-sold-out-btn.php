1e1f195302b914d88800db5bf2aa79647e8a5cc2
doxifieq <doxifieq@gmail.com>
2025-01-30 14:14:49 +0100

    [fix]

--- shortcodes/ew-sold-out-btn.php
+++ shortcodes/ew-sold-out-btn.php
@@ -24,10 +24,10 @@ function ew_sold_out_btn() {
 
         if ($ew_field_sold_out == true) {
             $output .= '<span class="ew-beer-status soldout">Udsolgt</span>';
-            $output .= '<a class="ew-beer-button" href="' . get_the_permalink() . '?set_in_stock=' . $ew_beer_ID . '">Meld udsolgt</a>';
+            $output .= '<a class="ew-beer-button" href="' . get_the_permalink() . '?set_in_stock=' . $ew_beer_ID . '">Meld på lager</a>';
         } else {
             $output .= '<span class="ew-beer-status instock">På lager</span>';
-            $output .= '<a class="ew-beer-button" href="' . get_the_permalink() . '?set_sold_out=' . $ew_beer_ID . '">Meld på lager</a>';
+            $output .= '<a class="ew-beer-button" href="' . get_the_permalink() . '?set_sold_out=' . $ew_beer_ID . '">Meld udsolgt</a>';
         }
 
         /*
