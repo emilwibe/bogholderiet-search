d1e20505a455e93d680713dd262d1d8a6e2c4e46
doxifieq <doxifieq@gmail.com>
2025-01-30 13:01:57 +0100

    null check?????????

--- shortcodes/ew-show-beers.php
+++ shortcodes/ew-show-beers.php
@@ -169,7 +169,9 @@ function ew_show_beers() {
             $beer_country = get_the_terms(get_the_ID(), 'beer_country');
             
             if( isset( $beer_country ) ) {
-                $output .= '<p>Land: ' . $beer_country[0]->name . '<br>';
+                if ($beer_country[0]) {
+                    $output .= '<p>Land: ' . $beer_country[0]->name . '<br>';
+                }
             }
 
             // OUTPUT BEER TYPE FROM CAT
