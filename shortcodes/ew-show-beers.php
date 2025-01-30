5dc19916c778286a18c463aedfaf2222f8dcb978
doxifieq <doxifieq@gmail.com>
2025-01-30 12:04:02 +0100

    [minecraft lets play] welcome to episode 1 guys

--- shortcodes/ew-show-beers.php
+++ shortcodes/ew-show-beers.php
@@ -153,7 +153,7 @@ function ew_show_beers() {
                 $output .= '<img src="' . plugin_dir_url( __FILE__ ) . '../assets/bogholderiet-placeholder-beer.png?new2" class="ew-no-img">';
             }
 
-            if (get_field('ew_sold_out') == 'Udsolgt') {
+            if (get_field('ew_sold_out') == true) {
                 $output .= "UDSOLGT DIN SPASSER";
             }
 
