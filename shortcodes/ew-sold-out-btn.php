1b3ab88edb4bf0abdfceb69e8390254922eaafe0
doxifieq <doxifieq@gmail.com>
2025-01-31 10:21:52 +0100

    [update] remove variable

--- shortcodes/ew-sold-out-btn.php
+++ shortcodes/ew-sold-out-btn.php
@@ -12,10 +12,8 @@ function ew_sold_out_btn() {
         } elseif ( isset($_GET['set_in_stock']) ) {
             update_field('ew_sold_out', [], $ew_beer_ID);
         }
-
-        $ew_field_sold_out = get_field('ew_sold_out');
-
-        if ( $ew_field_sold_out ) {
+        
+        if ( get_field('ew_sold_out') ) {
             $output .= '<span class="ew-beer-status soldout">Udsolgt</span>';
             $output .= '<a class="ew-beer-button" href="' . get_the_permalink() . '?set_in_stock=' . $ew_beer_ID . '">Meld på lager</a>';
         } else {
