c7bf01451c20c47e8d83c9776910013f46b36f5b
doxifieq <doxifieq@gmail.com>
2025-01-31 11:41:04 +0100

    update

--- shortcodes/ew-show-beers.php
+++ shortcodes/ew-show-beers.php
@@ -110,7 +110,7 @@ function ew_show_beers() {
 
     if ( is_user_logged_in() ) {
         if ( isset($_GET['sort']) ) {
-            $args['meta_value'] = '1';
+            $args['meta_value'] = ['1'];
         } else {
             unset($args['meta_key']);
             unset($args['meta_value']);
