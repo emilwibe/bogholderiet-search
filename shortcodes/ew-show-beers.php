9622ebe3757484d0c1c679f1fbd9687493a25adb
doxifieq <doxifieq@gmail.com>
2025-02-03 09:25:16 +0100

    [remove vardump]

--- shortcodes/ew-show-beers.php
+++ shortcodes/ew-show-beers.php
@@ -170,7 +170,6 @@ function ew_show_beers() {
             }
 
             if ( get_field( 'ew_sold_out' ) ) {
-                var_dump(get_field('ew_sold_out'));
                 $output .= '<div class="ew-beer-sold-out">';
 
                 $output .= '<h1>UDSOLGT</h1>';
